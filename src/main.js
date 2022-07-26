5d71ce984f54c05af941d4290213f7e557bc8467
lv <616393764@qq.com>
2022-07-26 16:07:03 +0800

    首页,仓库管理静态页面完成

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/reset.css'
+import './styles/font/iconfont.css'
+import * as echarts from 'echarts'
+
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+import './element-variables.scss'
+Vue.use(ElementUI)
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    component: () => import('@/views/login')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/',
+    component: () => import('@/views/home'),
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/home/components/home-main')
+      },
+      {
+        path: 'manage-base-info',
+        component: () => import('@/views/manage-base-info')
+      }
+    ]
   }
 ]
 
